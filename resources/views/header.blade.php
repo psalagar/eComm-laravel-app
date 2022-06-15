94b4601296462a86ba2996c35533b5d67c93a0cc
psalagar <prajakta.salagar93@gmail.com>
2022-06-15 16:26:20 +0530

    Login Logout logic

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -13,7 +13,7 @@ $total =  (Session::has('user'))? ProductController::cartItem() : 0;
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Brand</a>
+      <a class="navbar-brand" href="/">E-Comm</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
@@ -31,6 +31,17 @@ $total =  (Session::has('user'))? ProductController::cartItem() : 0;
       </form>
       <ul class="nav navbar-nav navbar-right">
         <li><a href="#">Cart ({{$total}})</a></li>
+        @if(Session::has('user'))
+          <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">{{Session::get('user')['name']}}
+            <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="/logout">Logout</a></li>
+            </ul>
+          </li>
+          @else
+          <li><a href="/login">Login</a></li>
+          @endif
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
