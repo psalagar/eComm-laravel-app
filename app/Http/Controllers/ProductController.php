b0c2c3cc0a8326b37a7f4930e7beb856ae7ba554
psalagar <prajakta.salagar93@gmail.com>
2022-06-15 17:32:08 +0530

    Cart list page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -39,4 +39,21 @@ class ProductController extends Controller
         $userId = Session::get('user')['id'];
         return Cart::where('user_id', $userId)->count();
     }
+
+    public function cartList() {
+        $userId = Session::get('user')['id'];
+        $products = DB::table('cart')
+        ->join('products', 'cart.product_id', '=', 'products.id')
+        ->where('cart.user_id', $userId)
+        ->select('products.*', 'cart.id as cart_id')
+        ->get();
+
+        return view('cartlist', ['products'=> $products]);
+    }
+    
+    public function removeCart($id) {
+        Cart::destroy($id);
+
+        return redirect('cartlist');
+    }
 }
